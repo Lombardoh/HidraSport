9200c992cbc3e4c565424d87b57f0d7a6e706208
unknown <lombardo.h@hotmail.com>
2020-10-20 23:26:49 -0300

    fixed more pathing, images, menu

--- register/views.py
+++ register/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from store.models import Product, Categorias, Secciones, Images
 
 # Create your views here.
 def register(response):
+    destacadas=Categorias.objects.filter(destacada=True)
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
@@ -11,5 +13,8 @@ def register(response):
         return redirect("http://127.0.0.1:8000/")
     else:
         form = RegisterForm()
-
-    return render(response, 'register/register.html', {"form":form})
+        
+    context = {
+        "form":form,
+        'destacadas':destacadas}
+    return render(response, 'register/register.html', context)
