4de2af67c51a0c2cc7a39dcdbecb6dc775cd3169
hernan lombardo <hernan.lombardo@sandvik.com>
2020-11-19 18:36:08 -0300

    added product view and select products from category admin

---
+++ hidrasport/register/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from store.models import Product, Categorias, Secciones, Images
+
+# Create your views here.
+def register(response):
+    destacadas=Categorias.objects.filter(destacada=True)
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        
+        return redirect("/")
+    else:
+        form = RegisterForm()
+        
+    context = {
+        "form":form,
+        'destacadas':destacadas}
+    return render(response, 'register/register.html', context)
+
+
+    
---
+++ hidrasport/store/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from store.models import Product, Categorias, Secciones, Images
+
+# def function name
+
+def index(request):
+    secciones = Secciones.objects.all()
+    lomejor = Product.objects.filter(categorias__nombre__contains="lomejor")
+    destacadas=Categorias.objects.filter(destacada=True)
+    context = {
+        "secciones": secciones,
+        "lomejor": lomejor,
+        "destacadas": destacadas
+    }
+    return render(request, 'index.html', context)
+
+def filtro(request, categoria):
+    articulos = Product.objects.filter( categorias__nombre__contains=categoria).order_by('name')
+    destacadas=Categorias.objects.filter(destacada=True)
+    banner=Categorias.objects.filter(nombre=categoria)
+    context = {
+        "categoria": categoria, 
+        "articulos": articulos,
+        "destacadas": destacadas,
+        "banner": banner
+    }
+    return render(request, "filtro.html", context)
+
+def filtro_images(request, pk):
+    images = Images.objects.filter(product=pk)
+    
+    context = {
+        'images':images
+    }
+    return (request, 'filtro.html', context)
+
+def articulo_detalle(request, pk):
+    articulo = Product.objects.get(pk=pk)
+    images = Images.objects.filter(product=pk)
+    destacadas=Categorias.objects.filter(destacada=True)
+    context = {
+        'articulo': articulo,
+        "destacadas": destacadas,
+        "images": images 
+    }
+    return render(request, 'articulo_detalle.html', context)
