2069f7d505030d798e8bf241eb53e048fca338a1
unknown <lombardo.h@hotmail.com>
2020-12-14 15:36:48 -0300

    fixed DBs, add remove duplicates action

--- store/admin.py
+++ store/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from store.models import Product, Categorias, Secciones, Images, Talles
 from store.resource import ImagesResource, TallesResource
+from django.db.models import Count, Max
 
 
 admin.site.site_header = "HidraSport Admin"
@@ -27,19 +28,39 @@ class CategoriastAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(ImportExportModelAdmin):
-    #resource_class = ProductResource
-    #list_display = ("name", "descripcion", "sexo", "color", "guard", "telas", "diseño", 'detalle_color', "image")
-    inlines = [ImagesAdmin, TallesAdmin]
     
-    def lista_categorias(self, obj):
-        return ", ".join([str(p) for p in obj.categorias.all()])
-#    pass
+    list_display = ("name", "descripcion", "sexo", "color", "guard", "telas", "diseño", 'detalle_color', "image",)
+    inlines = [ImagesAdmin, TallesAdmin]
+    actions = ['remover_duplicados',]
+    
+    def remover_duplicados(self, request, queryset):
+        unique_fields = ['name', 'descripcion','sexo', 'color', 'guard', 'diseño', 'detalle_color']
+
+        duplicates = (
+        Product.objects.values(*unique_fields)
+        .order_by()
+        .annotate(max_id=Max('id'), count_id=Count('id'))
+        .filter(count_id__gt=1)
+        )
+
+        for duplicate in duplicates:
+            (
+                Product.objects
+                .filter(**{x: duplicate[x] for x in unique_fields})
+                .exclude(id=duplicate['max_id'])
+                .delete()
+            )
+    
+    #def lista_categorias(self, obj):
+    #    return ", ".join([str(p) for p in #obj.categorias.all()])
+
 
 @admin.register(Talles)
 class TallesAdmin(ImportExportModelAdmin):
     resource_class = TallesResource
     list_display = ('id', 'product', 'talle', 'cantidad')
     pass
+
 
 @admin.register(Images)
 class ImagesAdmin(ImportExportModelAdmin):
@@ -49,3 +70,8 @@ class ImagesAdmin(ImportExportModelAdmin):
 
 admin.site.register(Categorias, CategoriastAdmin)
 admin.site.register(Secciones, SeccionesAdmin)
+
+#---------- admin actions
+
+
+
--- store/views.py
+++ store/views.py
@@ -10,23 +10,6 @@ def index(request):
     lomejor = Product.objects.filter(categorias__nombre__contains="lomejor")
     destacadas=Categorias.objects.filter(destacada=True)
     
-    unique_fields = ['name', 'descripcion','sexo', 'color', 'guard', 'diseño', 'detalle_color']
-
-    duplicates = (
-    Product.objects.values(*unique_fields)
-    .order_by()
-    .annotate(max_id=Max('id'), count_id=Count('id'))
-    .filter(count_id__gt=1)
-    )
-
-    for duplicate in duplicates:
-        (
-        Product.objects
-        .filter(**{x: duplicate[x] for x in unique_fields})
-        .exclude(id=duplicate['max_id'])
-        .delete()
-    )
-    
     context = {
         "secciones": secciones,
         "lomejor": lomejor,
