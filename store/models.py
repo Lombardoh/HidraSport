1f0299ca4500943be16e895589924d8ac28a78ba
hernan lombardo <hernan.lombardo@sandvik.com>
2021-01-26 11:26:42 -0300

    making sure I committed everything

--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Categorias(models.Model):
     nombre = models.CharField(max_length=20)
     destacada = models.BooleanField(default=False)
-#    imagen = models.ImageField(upload_to='images/')
+    imagen = models.ImageField(upload_to='images/')
     
     def __str__(self):
         return self.nombre
