458a637183263a6d45265704a3aa4e77210256dd
unknown <lombardo.h@hotmail.com>
2021-05-23 11:42:25 -0300

    pushed all changes in visual and cart

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,17 @@
+from register.models import Profile
+from django.contrib.admin.options import TabularInline
+from django.db import models
+from orders.models import User
 from django.contrib import admin
-
+from register.models import Profile
 # Register your models here.
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    inlines =[ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
